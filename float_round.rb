c6685d92d7d92bbf89fafb50a70de1ae770aa262
rixiobarrios <rixiobarrios.consulado@gmail.com>
2022-09-28 16:11:14 +0000

    completed

--- float_round.rb
+++ float_round.rb
@@ -1,13 +1,23 @@
 # Output ten divided by 3 rounded to 3 decimal places.
 
+# num1 = 10
+# num2 = 3
+
 # num1 = 10.00
 # num2 = 3.00
 
-num1 = 10
-num2 = 3
+num1 = 10.to_f
+num2 = 3.to_f
 
-div = num1 / num2
+# div = (num1 / num2)
+# div = (num1/num2)
 
-puts div.round(3).to_i
+# puts div.round(3).to_i
+# puts div.round(3).to_f
+# puts div.round(3).to_s
+# print div.round(3)
+# p div.round(3)
 
-# output 3.333
+p (num1/num2).round(3)
+
+
